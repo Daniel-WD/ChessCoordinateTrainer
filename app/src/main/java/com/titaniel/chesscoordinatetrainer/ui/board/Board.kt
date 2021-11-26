65cc368c7b051959e460cab4cc60b96a2b36284c
Daniel Weidensdoerfer <daniel.weidensdoerfer@gmail.com>
2021-11-26 17:07:06 +0100

    Add chess pieces

--- app/src/main/java/com/titaniel/chesscoordinatetrainer/ui/board/Board.kt
+++ app/src/main/java/com/titaniel/chesscoordinatetrainer/ui/board/Board.kt
@@ -1,19 +1,61 @@
 package com.titaniel.chesscoordinatetrainer.ui.board
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.titaniel.chesscoordinatetrainer.R
 
 enum class ChessColor {
     BLACK,
     WHITE
 }
+
+/**
+ * Maps each piece notation to its respective icon
+ */
+private val pieceIdByNotation = mapOf(
+    "a1" to R.drawable.ic_rook_white,
+    "b1" to R.drawable.ic_knight_white,
+    "c1" to R.drawable.ic_bishop_white,
+    "d1" to R.drawable.ic_queen_white,
+    "e1" to R.drawable.ic_king_white,
+    "f1" to R.drawable.ic_bishop_white,
+    "g1" to R.drawable.ic_knight_white,
+    "h1" to R.drawable.ic_rook_white,
+    "a2" to R.drawable.ic_pawn_white,
+    "b2" to R.drawable.ic_pawn_white,
+    "c2" to R.drawable.ic_pawn_white,
+    "d2" to R.drawable.ic_pawn_white,
+    "e2" to R.drawable.ic_pawn_white,
+    "f2" to R.drawable.ic_pawn_white,
+    "g2" to R.drawable.ic_pawn_white,
+    "h2" to R.drawable.ic_pawn_white,
+    "a8" to R.drawable.ic_rook_black,
+    "b8" to R.drawable.ic_knight_black,
+    "c8" to R.drawable.ic_bishop_black,
+    "d8" to R.drawable.ic_king_black,
+    "e8" to R.drawable.ic_queen_black,
+    "f8" to R.drawable.ic_bishop_black,
+    "g8" to R.drawable.ic_knight_black,
+    "h8" to R.drawable.ic_rook_black,
+    "a7" to R.drawable.ic_pawn_black,
+    "b7" to R.drawable.ic_pawn_black,
+    "c7" to R.drawable.ic_pawn_black,
+    "d7" to R.drawable.ic_pawn_black,
+    "e7" to R.drawable.ic_pawn_black,
+    "f7" to R.drawable.ic_pawn_black,
+    "g7" to R.drawable.ic_pawn_black,
+    "h7" to R.drawable.ic_pawn_black
+)
 
 @Composable
 fun ChessBoard(boardColorFront: ChessColor = ChessColor.BLACK, onTileClick: (String) -> Unit = {}) {
@@ -26,7 +68,7 @@ fun ChessBoard(boardColorFront: ChessColor = ChessColor.BLACK, onTileClick: (Str
 
     Column(
         modifier = Modifier
-            .background(MaterialTheme.colors.onBackground.copy(alpha = 0.14f))
+            .background(MaterialTheme.colors.onBackground.copy(alpha = 0.15f))
             .padding(6.dp)
     ) {
 
@@ -54,15 +96,22 @@ fun BoardTile(type: ChessColor, notation: String, onClick: (String) -> Unit) {
             .size(42.dp)
             .background(
                 when (type) {
-                    ChessColor.BLACK -> if (MaterialTheme.colors.isLight) Color(0xFF989898) else MaterialTheme.colors.background
-                    else -> if (MaterialTheme.colors.isLight) Color.White else Color(0xFF5E5E5E)
+                    ChessColor.BLACK -> if (MaterialTheme.colors.isLight) Color(0xFFCACACA) else Color(
+                        0xFF414141
+                    )
+                    else -> if (MaterialTheme.colors.isLight) Color.White else Color(0xFF646464)
                 }
             )
             .clickable {
                 onClick(notation)
             }
     ) {
-        //Text(text = notation)
+        pieceIdByNotation[notation]?.let { pieceId ->
+
+            Image(modifier = Modifier.padding(1.dp), painter = painterResource(id = pieceId), contentDescription = null)
+        }
+
+//        Text(text = notation)
     }
 }
 
